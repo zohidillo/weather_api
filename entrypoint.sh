1c251ca3252cd38cad020008bd87b17af03cb83b
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2025-03-14 17:43:23 +0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-export DJANGO_SETTINGS_MODULE=config.settings.development
+export DJANGO_SETTINGS_MODULE=config.settings.deployment
 
 python manage.py install
 python manage.py runserver 0.0.0.0:8080
